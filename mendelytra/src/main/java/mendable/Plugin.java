9a791123d793dde2ebc2a2e19c6967d425edc4b8
SHC <47350223+yumenonikki@users.noreply.github.com>
2019-05-25 20:26:01 +0900

    test commit

---
+++ mendelytra/src/main/java/mendable/Plugin.java
@@ -0,0 +1,37 @@
+package mendable;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerExpChangeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin implements Listener{
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onExpChange(PlayerExpChangeEvent event) {
+		ItemStack item = event.getPlayer().getInventory().getChestplate();
+		if(item == null) {
+			return;
+		}
+		if(item.getType() == Material.ELYTRA) {
+			ItemMeta meta = item.getItemMeta();
+			int damage = ((Damageable) meta).getDamage();
+
+			if(damage != 0) {
+				int mend = damage - event.getAmount();
+				((Damageable) meta).setDamage(mend);
+				item.setItemMeta(meta);
+				event.getPlayer().getInventory().setChestplate(item);
+			}
+		}
+	}
+}
